5c27d41780fcc38d7b02bdf820eba6f2de6220c4
Twist <twistmikan@gmail.com>
2026-01-15 19:55:53 +0800

    拟合data4完成

--- 1-2-homework/main.py
+++ 1-2-homework/main.py
@@ -46,6 +46,27 @@ class SimpleMLP3(nn.Module):
         # 第3层全连接后直接输出
         return self.fc3(x)
 
+class SimpleMLP4(nn.Module):
+    def __init__(self):
+        super(SimpleMLP4, self).__init__()
+
+        # 创建两个全连接层
+        self.fc1 = nn.Linear(2, 32)
+        self.fc2 = nn.Linear(32, 32)
+        self.fc3 = nn.Linear(32, 1)
+
+    def forward(self, x):
+        # 第1层全连接后使用ReLU激活函数
+        x = self.fc1(x)
+        x = F.relu(x)
+
+        # 第2层全连接后使用ReLU激活函数
+        x = self.fc2(x)
+        x = F.relu(x)
+
+        # 第3层全连接后直接输出
+        return self.fc3(x)
+
 def fit_data3():
     # 在这里读入不同的 csv 文件
     X, Y = utils.read_csv_data('data_3.csv')
@@ -92,8 +113,52 @@ def fit_data3():
     utils.draw_2d_scatter(X, Y, predicted.detach().numpy())
     # utils.draw_3d_scatter(X, Y, predicted.detach().numpy())
 
-# def fit_data4():
-#
+def fit_data4():
+    # 在这里读入不同的 csv 文件
+    X, Y = utils.read_csv_data('data_4.csv')
+
+    X = torch.tensor(X, dtype=torch.float32)
+    Y = torch.tensor(Y, dtype=torch.float32)
+    print(X.shape, Y.shape)
+
+    # 根据特征维度，绘制 2D 或 3D 散点图
+    # utils.draw_2d_scatter(X, Y)
+    utils.draw_3d_scatter(X, Y)
+
+    # 模型、损失函数、优化器
+    model = SimpleMLP4()
+    criterion = nn.MSELoss()
+    optimizer = optim.Adam(model.parameters(), lr=0.01)
+
+    # 创建数据集和加载器
+    dataset = TensorDataset(X, Y)
+    data_loader = DataLoader(dataset, batch_size=16, shuffle=True)
+
+    # 训练模型
+    epochs = 200
+    for epoch in range(epochs):
+        epoch_loss = 0
+
+        for batch_x, batch_y in data_loader:
+            # 预测输出、计算损失
+            predictions = model(batch_x)
+            loss = criterion(predictions, batch_y)
+
+            # 计算梯度、更新参数
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            # 累积损失
+            epoch_loss += loss.item()
+
+        # 打印本轮的损失值
+        print(f'Epoch {epoch}, Loss: {epoch_loss / len(data_loader)}')
+
+    # 查看预测效果
+    predicted = model(X)
+    # utils.draw_2d_scatter(X, Y, predicted.detach().numpy())
+    utils.draw_3d_scatter(X, Y, predicted.detach().numpy())
 
 def demo():
     # 在这里读入不同的 csv 文件
@@ -144,5 +209,5 @@ def demo():
     utils.draw_3d_scatter(X, Y, predicted.detach().numpy())
 
 if __name__ == '__main__':
-    fit_data3()
-    # fit_data4()
+    # fit_data3()
+    fit_data4()
