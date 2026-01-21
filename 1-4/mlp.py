ed79b8d03f17f92890c8e12965c61aefd2e81a2b
Twist <twistmikan@gmail.com>
2026-01-21 09:18:40 +0800

    define mlp model

--- 1-4/mlp.py
+++ 1-4/mlp.py
@@ -6,4 +6,26 @@ import torch.nn.functional as F
 # 定义一个简单的多层感知机模型
 # pytorch 中所有自定义的神经网络类，都要继承 nn.Module
 class SimpleMLP(nn.Module):
-    # TODO: 开始你的表演，请不要直接复制代码！
+    def __init__(self) -> None:
+        super().__init__()
+        input_dim = 28 * 28
+        hidden_dim = 28 * 28 * 2
+        hidden_layers = 3
+        output_dim = 10
+
+        layers = []
+        dim = input_dim
+        for _ in range(hidden_layers):
+            layers.append(nn.Linear(dim, hidden_dim))
+            layers.append(nn.ReLU())
+            layers.append(nn.Dropout(p=0.5))
+            dim = hidden_dim
+        layers.append(nn.Linear(dim, output_dim))
+        layers.append(nn.Sigmoid())
+        self.net = nn.Sequential(*layers)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = x.flatten(start_dim=1)
+        
+        return self.net(x)
+    
